422a4e5a27bc1c09f6e08d4b156711a50166006a
Tony Yang <tianyuyang658@yahoo.com>
2019-03-27 22:53:54 -0700

    Refactor unit test assertion. Add test for http Error when using base controller is not allowed

--- webmvc/server.go
+++ webmvc/server.go
@@ -15,5 +15,9 @@ func (s *NewServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   requestPath := r.URL.Path
   controller := s.routes.GetController(requestPath)
   response := *controller.Get()
-  fmt.Fprint(w, response.Body);
+  if response.StatusCode == 200 {
+    fmt.Fprint(w, response.Body)
+  } else {
+    http.Error(w, response.Body, response.StatusCode)
+  }
 }
--- webmvc/server_test.go
+++ webmvc/server_test.go
@@ -10,19 +10,16 @@ import (
 
 func TestServerHello(t *testing.T) {
   routes := Routes{
-    // map[string]string{
-    //   "/index": "Hello Webmvc from Go",
-    //   "/hello": "Hello World!",
-    // },
     map[string]base.ControllerInterface{
       "/index": &controllers.Index{},
       "/hello": &controllers.Hello{},
+      "/base": &base.Controller{},
     },
   }
 
   server := &NewServer{&routes}
 
-  t.Run("return Hello Webmvc from index", func( t *testing.T) {
+  t.Run("return Hello Webmvc from index", func(t *testing.T) {
     request, _ := http.NewRequest(http.MethodGet, "/index", nil)
     response := httptest.NewRecorder()
 
@@ -30,13 +27,14 @@ func TestServerHello(t *testing.T) {
 
     got := response.Body.String()
     want := "Hello Webmvc from Go"
+    AssertStringEqual(t, got, want)
 
-    if got != want {
-      t.Errorf("got '%s', want '%s'", got, want)
-    }
+    gotCode := response.Code
+    wantCode := http.StatusOK
+    AssertIntEqual(t, gotCode, wantCode)
   })
 
-  t.Run("return Hello World from hello", func( t *testing.T) {
+  t.Run("return Hello World from hello", func(t *testing.T) {
     request, _ := http.NewRequest(http.MethodGet, "/hello", nil)
     response := httptest.NewRecorder()
 
@@ -44,9 +42,39 @@ func TestServerHello(t *testing.T) {
 
     got := response.Body.String()
     want := "Hello World!"
+    AssertStringEqual(t, got, want)
 
-    if got != want {
-      t.Errorf("got '%s', want '%s'", got, want)
-    }
+    gotCode := response.Code
+    wantCode := http.StatusOK
+    AssertIntEqual(t, gotCode, wantCode)
   })
+
+  t.Run("base controller should return 405 with Method not allowed", func(t *testing.T) {
+    request, _ := http.NewRequest(http.MethodGet, "/base", nil)
+    response := httptest.NewRecorder()
+
+    server.ServeHTTP(response, request)
+
+    got := response.Body.String()
+    want := "Method not allowed\n"
+    AssertStringEqual(t, got, want)
+
+    gotCode := response.Code
+    wantCode := 405
+    AssertIntEqual(t, gotCode, wantCode)
+  })
+}
+
+func AssertStringEqual(t *testing.T, got, want string) {
+  t.Helper()
+  if got != want {
+    t.Errorf("got string '%s', want '%s'", got, want)
+  }
+}
+
+func AssertIntEqual(t *testing.T, got, want int) {
+  t.Helper()
+  if got != want {
+    t.Errorf("got int '%d', want '%d'", got, want)
+  }
 }
